3922b4e83f1e832e575aadee9545d5430c37f9ee
key0123456789 <k.i-0123456789@live.jp>
2020-04-07 01:47:50 +0900

    moving cards

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class="list">
-    <div class="listheader">
-      <p class="list-title">{{ title }}</p>
-      <p class="list-counter">total: {{ totalCardInList }}</p>
-      <div class="deletelist" @click="removeList">✕</div>
-    </div>
-    <card v-for="(item, index) in cards"
-          :body="item.body"
-          :key="item.id"
-          :cardIndex="index"
-          :listIndex="listIndex"
-    />
-    <card-add :listIndex="listIndex"/>
+    <div class="list">
+      <div class="listheader">
+        <p class="list-title">{{ title }}</p>
+        <p class="list-counter">total: {{ totalCardInList }}</p>
+        <div class="deletelist" @click="removeList">✕</div>
+      </div>
+    <draggable group="cards" :list="cards" @end="$emit('change')">
+      <card v-for="(item, index) in cards"
+            :body="item.body"
+            :key="item.id"
+            :cardIndex="index"
+            :listIndex="listIndex"
+      />
+      <card-add :listIndex="listIndex"/>
+    </draggable>
   </div>
 </template>
 
 <script>
+import draggable from "vuedraggable"
 import CardAdd from "./CardAdd"
 import Card from "./Card"
 
 export default {
   components: {
+    draggable,
     CardAdd,
     Card
   },
@@ -36,7 +40,7 @@ export default {
     listIndex: {
       type: Number,
       required: true
-    }
+    },
   },
   computed: {
     totalCardInList: function() {
